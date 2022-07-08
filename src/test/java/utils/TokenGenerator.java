068be8493e6ff5b457397c152ee7d7a0948c85e4
sahil-100ms <105665560+sahil-100ms@users.noreply.github.com>
2022-07-08 08:32:52 +0000

    Deploying to gh-pages from @ 100mslive/karate-api-tests@e6d685822a478ea1b9876735c0165755d88c2179 🚀

--- src/test/java/utils/TokenGenerator.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class TokenGenerator {
-     public static String getToken() {
-
-         String token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoyLCJ0eXBlIjoibWFuYWdlbWVudCIsImFwcF9kYXRhIjpudWxsLCJhY2Nlc3Nfa2V5IjoiNjI4MjJkYjAwNWM2NDg3ZjllNTU2NDU2IiwiZXhwIjoxNjU3MDcwMzk2LCJqdGkiOiIyNmQ0Yzg1NS1kOWQ3LTRjNmEtYThkOS01YTc3MDE5MTU4MTYiLCJpYXQiOjE2NTcwMzQzOTYsImlzcyI6IjYyODIyZGFmMDVjNjQ4N2Y5ZTU1NjQ1MiIsIm5iZiI6MTY1NzAzNDM5Niwic3ViIjoiYXBpIn0.dyyfKBK4xrGn7NGait_q-vCVJIlVpzv5fbbKxY1-Wck";
-         return token;
-     }
-}
