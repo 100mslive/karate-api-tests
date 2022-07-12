f891ec9e741cf6f69f8d77cb19551f4a75750290
Sahil Kashyap <sahil@100ms.live>
2022-07-12 10:37:05 +0530

    added token generator

--- src/test/java/utils/TokenGenerator.java
+++ src/test/java/utils/TokenGenerator.java
@@ -1,10 +1,27 @@
 package utils;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class TokenGenerator {
      public static String getToken() {
-
-         String token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoyLCJ0eXBlIjoibWFuYWdlbWVudCIsImFwcF9kYXRhIjpudWxsLCJhY2Nlc3Nfa2V5IjoiNjJjNjk2YTk4YzllN2Y3Nzk2Y2Q5ZWM2IiwiZXhwIjoxNjY1ODIyMDYzLCJqdGkiOiI3NDU3YWIwMC1iYzdiLTQzMWItYmY0MS04ZmE0ODU2YWQyMjkiLCJpYXQiOjE2NTcxODIwNjMsImlzcyI6IjYyYzY5NmE4OGM5ZTdmNzc5NmNkOWVjMiIsIm5iZiI6MTY1NzE4MjA2Mywic3ViIjoiYXBpIn0.AhP_c5v-m1HZN6yqZ6s3R6Nu23-cfGhBzl2SSlUOp_A";
-         return token;
+         String appConstant = "26nL6KVZ2u3MODHNE4ugxf0mEQA-0t0W-CxOSSAOKFl5Waq0QkH9XGtImbEo9sgM2oe3eerzMrcM8xCfqujK-jIfncwUFt6BabgB49ELmLFnXO35h29mdav9N2d8TCG8r5-5DmGT2wiNDQZDe3NwmM0dp4HCWSiJn9SpIuiUpxc=";
+         Map<String, Object> payload = new HashMap<>();
+         payload.put("access_key", "62822daf05c6487f9e556455");
+         payload.put("type", "management");
+         payload.put("version", 2);
+         String token = Jwts.builder().setClaims(payload).setId(UUID.randomUUID().toString())
+                 .setExpiration(new Date(System.currentTimeMillis() + 86400 * 1000))
+                 .setIssuedAt(Date.from(Instant.ofEpochMilli(System.currentTimeMillis() - 60000)))
+                 .setNotBefore(new Date(System.currentTimeMillis()))
+                 .signWith(SignatureAlgorithm.HS256, appConstant.getBytes()).compact();
+         return "Bearer " + token;
      }
 
      public static int getRandomNumber(){
